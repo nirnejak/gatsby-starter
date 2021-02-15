3bf6e447aaea09e9fd2544053720d1b74296223e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2021-02-16 00:31:54 +0530

    Layout Component Updated

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { StaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import PropTypes from "prop-types"
 
@@ -8,22 +8,19 @@ import "../assets/scss/main.scss"
 
 import "./index.scss"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={(data) => {
-      return <main>{children}</main>
-    }}
-  />
-)
+    }
+  `)
+
+  return <main>{children}</main>
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
